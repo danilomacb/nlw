d32118f077be96075560d8d56ef43976b0d53755
danilomacb <danilo.bakun@gmail.com>
2020-06-02 15:47:18 -0300

    points post

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,12 +9,40 @@ routes.get("/items", async (req, res) => {
 
   const serializedItems = items.map((item) => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
     };
   });
 
   return res.json(serializedItems);
+});
+
+routes.post("/points", async (req, res) => {
+  const { name, email, whatsapp, latitude, longitude, city, uf, items } = req.body;
+
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx("points").insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const point_id = insertedIds[0];
+
+  const point_items = items.map((item_id: number) => {
+    return { item_id, point_id };
+  });
+
+  await trx("point_items").insert(point_items);
+
+  return res.json({ success: true });
 });
 
 export default routes;
