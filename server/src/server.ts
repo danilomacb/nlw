c25a9772a3a1999ff6ed792b28e457567be7affa
danilomacb <danilo.bakun@gmail.com>
2020-06-02 09:46:34 -0300

    creating test routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,35 @@ import express from "express";
 
 const app = express();
 
-app.get("/users", (req, res) => {
-  console.log("Listagem de usuarios");
+app.use(express.json());
 
-  res.json(["Diego", "Cleiton", "Danilo", "Daniel"]);
+const users = ["Diego", "Cleiton", "Danilo", "Daniel"];
+
+app.get("/users", (req, res) => {
+  const search = String(req.query.search);
+
+  const filteredUsers = search ? users.filter((user) => user.includes(search)) : users;
+
+  return res.json(filteredUsers);
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const user = users[id];
+
+  return res.json(user);
+});
+
+app.post("/users", (req, res) => {
+  const data = req.body;
+
+  const user = {
+    name: data.name,
+    email: data.email,
+  };
+
+  return res.json(user);
 });
 
 app.listen(3333);
