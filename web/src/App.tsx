9bfbe0136ef9dc0be8b875a2b2c2959151e4a063
danilomacb <danilo.bakun@gmail.com>
2020-06-04 08:49:16 -0300

    state

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 import Header from "./Header";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
   return (
     <div>
-      <Header title="Hello World" />
+      <Header title={`Contador: ${counter}`} />
 
-      <h1>Conteudo da aplicacao</h1>
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>
+        Aumentar
+      </button>
     </div>
   );
 }
