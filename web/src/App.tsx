3af69c4aa5e59efc6da4e731195b6bbf9134b162
danilomacb <danilo.bakun@gmail.com>
2020-06-04 08:14:26 -0300

    creating header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+
 import "./App.css";
+import Header from "./Header";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Header />
+
+      <h1>Conteudo da aplicacao</h1>
+    </div>
+  );
 }
 
 export default App;
