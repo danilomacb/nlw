a5127597b19d7af6d3a10d746aa2ae3eefbcc755
danilomacb <danilo.bakun@gmail.com>
2020-06-06 20:27:38 -0300

    image upload

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -19,7 +19,15 @@ interface Params {
 }
 
 interface Data {
-  point: { image: string; name: string; email: string; whatsapp: string; city: string; uf: string };
+  point: {
+    image: string;
+    image_url: string;
+    name: string;
+    email: string;
+    whatsapp: string;
+    city: string;
+    uf: string;
+  };
   items: { title: string }[];
 }
 
@@ -63,7 +71,7 @@ const Detail = () => {
           <Icon name="arrow-left" size={20} color="#34cd79" />
         </TouchableOpacity>
 
-        <Image style={styles.pointImage} source={{ uri: data.point.image }} />
+        <Image style={styles.pointImage} source={{ uri: data.point.image_url }} />
 
         <Text style={styles.pointName}>{data.point.name}</Text>
         <Text style={styles.pointItems}>{data.items.map((item) => item.title).join(", ")}</Text>
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -8,6 +8,7 @@ import { LeafletMouseEvent } from "leaflet";
 import "./styles.css";
 import logo from "../../assets/logo.svg";
 import api from "../../services/api";
+import Dropzone from "../../components/Dropzone";
 
 interface Item {
   id: number;
@@ -40,6 +41,7 @@ const CreatePoint = () => {
     whatsapp: "",
   });
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
 
@@ -129,16 +131,18 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
+
+    data.append("name", name);
+    data.append("email", email);
+    data.append("whatsapp", whatsapp);
+    data.append("uf", uf);
+    data.append("city", city);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("items", items.join(","));
+
+    if (selectedFile) data.append("image", selectedFile);
 
     await api.post("points", data);
 
@@ -162,6 +166,8 @@ const CreatePoint = () => {
         <h1>
           Cadastro do <br /> ponto de coleta
         </h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
